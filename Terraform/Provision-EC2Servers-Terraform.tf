82958848e722023d050d72eb27700baf2db18391
DevOpsPOC <rhedil.gomez@gmail.com>
2018-08-26 21:18:07 -0600

    update: changed the access key and secret to a false one(security reason

--- Terraform/Provision-EC2Servers-Terraform.tf
+++ Terraform/Provision-EC2Servers-Terraform.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-  access_key = "AKIAIFG4DXQRB3K4LU5Q"
-  secret_key = "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"
-  region     = "us-east-2"
+  access_key = "TESfdsfsdAKIAID3TESfdsfsdAKIAID3Efadsfasdfsf3AK3BUN4PCWAa"
+  secret_key = "adsfsDfdgfdgdfgfdj4+jadsfasfsa1m//fVq2CLdK1X7VSnBb99hIGSgkWAIv3TF"
+  region     = "us-east-1"
 }
+
 resource "aws_security_group" "ssh_and_http" {
   name = "allow_ssh_and_http"
   description = "Allow SSH and HTTP traffic"
@@ -35,6 +36,15 @@ resource "aws_security_group" "ssh_and_http" {
       cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+      from_port = 3000
+      to_port = 3000
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+
   egress {
       from_port = 0
       to_port = 0
@@ -43,31 +53,31 @@ resource "aws_security_group" "ssh_and_http" {
   }
 }
 
-resource "aws_instance" "jenkins_master_server" {
-  ami           = "ami-b0b7e3dc"
+resource "aws_instance" "jenkins_master_server_centos" {
+  ami           = "ami-4bf3d731"
   instance_type = "t2.small"
   security_groups = ["${aws_security_group.ssh_and_http.name}"]
   key_name = "RhedsAWSPublickey"
   tags {
-        Name = "jenkins_master_server"
-        role = "jenkins_master_server"
+        Name = "jenkins_master_server_centos"
+        role = "jenkins_master_server_centos"
        }
 
 }
 
-resource "aws_instance" "swarm-docker_server" {
-  ami           = "ami-b0b7e3dc"
+resource "aws_instance" "swarm_docker_server_centos" {
+  ami           = "ami-4bf3d731"
   instance_type = "t2.small"
   security_groups = ["${aws_security_group.ssh_and_http.name}"]
   key_name = "RhedsAWSPublickey"
   tags {
-        Name = "swarm-docker_server"
+        Name = "swarm_docker_server_centos"
         role = "swarm-docker_server"
        }
 
 }
 
-resource "aws_eip" "jenkins_master_server" {
-	instance = "${aws_instance.jenkins_master_server.id}"    
+resource "aws_eip" "jenkins_master_server_centos" {
+	instance = "${aws_instance.jenkins_master_server_centos.id}"
 	vpc = true
 }
